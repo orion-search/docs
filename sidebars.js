73e53889c627c54b496dcf6419f9431834aa5518
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-08-19 10:10:06 +0100

    Fix edit links and organise sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: {
       Overview: ['Introduction', 'Data Sources', 'Research Indicators', 'External Dependencies', 'Contributing', 'Use Cases'],
-      Components: [{ETL: ['Data Pipeline', 'The model config file', 'Operators']}],
+      Components: [{ETL: ['Data Pipeline', 'The model config file', 'Operators'], 'Search engine': ['Overview', 'Elasticsearch', 'Semantic Search']}],
       Tutorials: ['running_etl'],
   },
 };
